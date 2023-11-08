e6a34f82c952f438d8e9437724306732dc6effc3
darrpama <darrpama@student.21-school.ru>
2023-11-08 21:22:45 +0300

    repaired and checked customer view

--- src/part2-1-customers.sql
+++ src/part2-1-customers.sql
@@ -1,6 +1,7 @@
+-- Connect to the database
 \connect "dbname=retail_analytics user=retail_user";
 
-CALL import_default_dataset();
+CALL import_default_dataset_mini();
 
 -- Get last analysis date
 CREATE OR REPLACE FUNCTION get_last_analysis_date()
@@ -85,8 +86,8 @@ CREATE OR REPLACE VIEW Customers
 AS (
     WITH transaction_info_table AS (
         SELECT
-            cards.customer_id,
-            AVG(t.transaction_summ::numeric) AS customer_average_check,
+            personal_information.customer_id,
+            COALESCE(AVG(t.transaction_summ::numeric), 0.0) AS customer_average_check,
             get_interval_between_dates(
                 MIN(t.transaction_datetime), 
                 MAX(t.transaction_datetime)
@@ -96,18 +97,18 @@ AS (
                 get_last_analysis_date()
             ) AS customer_inactive_period
         FROM personal_information
-            JOIN cards ON personal_information.customer_id = cards.customer_id
-            JOIN transactions as t ON cards.card_id = t.card_id
-        GROUP BY cards.customer_id
+            LEFT JOIN cards ON personal_information.customer_id = cards.customer_id
+            LEFT JOIN transactions as t ON cards.card_id = t.card_id
+        GROUP BY personal_information.customer_id
     ),
 
     rank_table AS (
         SELECT
             customer_id,
             customer_average_check,
-            CUME_DIST() OVER (ORDER BY customer_average_check) AS rank_check,
+            (ROW_NUMBER() OVER (ORDER BY customer_average_check DESC))::NUMERIC / (SELECT COUNT(*) FROM personal_information) AS rank_check,
             customer_frequency,
-            CUME_DIST() OVER (ORDER BY customer_frequency) AS rank_frequency,
+            (ROW_NUMBER() OVER (ORDER BY customer_frequency DESC))::NUMERIC / (SELECT COUNT(*) FROM personal_information) AS rank_frequency,
             customer_inactive_period,
             customer_inactive_period / customer_frequency AS customer_churn_rate
         FROM transaction_info_table
@@ -125,13 +126,15 @@ AS (
                 WHEN rank_check <= 0.35 THEN 'Medium'
                 ELSE 'Low' END AS customer_average_check_segment,
             CASE
-                WHEN rank_frequency <= 0.1 THEN 'Often'
-                WHEN rank_frequency <= 0.35 THEN 'Occasionally'
+                WHEN rank_frequency > 0.9 THEN 'Often'
+                WHEN rank_frequency > 0.65 THEN 'Occasionally'
                 ELSE 'Rarely' END AS customer_frequency_segment,
             CASE
                 WHEN customer_churn_rate <= 2 THEN 'Low'
                 WHEN customer_churn_rate <= 5 THEN 'Medium'
-                ELSE 'High' END AS customer_churn_segment
+                WHEN customer_churn_rate IS NULL THEN 'High'
+                ELSE 'High' END AS customer_churn_segment,
+            rank_frequency
         FROM rank_table
     )
 
@@ -156,7 +159,8 @@ AS (
             WHEN 'Low' THEN 1
             WHEN 'Medium' THEN 2
             ELSE 3 END AS customer_segment,
-        get_primary_store_id(t.customer_id) AS customer_primary_store
+        get_primary_store_id(t.customer_id) AS customer_primary_store,
+        t.rank_frequency
     FROM stat_segment t
     ORDER BY t.customer_id
 );  -- Customer view end
