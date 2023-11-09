7a6c61f23033cc91547b4cdcca6a8b2e0bc045e4
timofann <dedelmir@student.21-school.ru>
2023-11-09 20:24:44 +0300

    feature: using filter

--- src/part2-4-groups.sql
+++ src/part2-4-groups.sql
@@ -5,45 +5,8 @@
 
 -- );
 
-INSERT INTO retail_analitycs_config VALUES (
-    DEFAULT, 'groups_margin_calculation_method', 'by all transactions',
-    'Three options are available: "by period" or "by number of transactions" or "by all transactions" (default)');
-
-INSERT INTO retail_analitycs_config VALUES (
-    DEFAULT, 'groups_margin_days_from_analysis_formation', '356',
-    'If the option "groups_margin_calculation_method" is set to "by period"');
-
-INSERT INTO retail_analitycs_config VALUES (
-    DEFAULT, 'groups_margin_number_of_transactions', '5',
-    'If the option "groups_margin_calculation_method" is set to "by number of transactions"');
-
--- this can be useful during the check
--- UPDATE retail_analitycs_config 
---     SET setting = 'by all transactions' 
---     WHERE name = 'groups_margin_calculation_method';
--- SELECT * FROM retail_analitycs_config;
--- SELECT * FROM get_groups_transactions();
-
--- UPDATE retail_analitycs_config 
---     SET setting = 'by number of transactions' 
---     WHERE name = 'groups_margin_calculation_method';
--- UPDATE retail_analitycs_config 
---     SET setting = '5' 
---     WHERE name = 'groups_margin_number_of_transactions';
--- SELECT * FROM retail_analitycs_config;
--- SELECT * FROM get_groups_transactions();
-
--- UPDATE retail_analitycs_config 
---     SET setting = 'by period' 
---     WHERE name = 'groups_margin_calculation_method';
--- UPDATE retail_analitycs_config 
---     SET setting = '365' 
---     WHERE name = 'groups_margin_days_from_analysis_formation';
--- SELECT * FROM retail_analitycs_config;
--- SELECT * FROM get_groups_transactions();
-
 DROP FUNCTION IF EXISTS get_groups_transactions;
-CREATE OR REPLACE FUNCTION get_groups_transactions() 
+CREATE FUNCTION get_groups_transactions() 
 RETURNS TABLE (
     customer_id             BIGINT,
     transaction_id          BIGINT,
@@ -109,120 +72,196 @@ BEGIN
 END $$
 LANGUAGE plpgsql;
 
+SELECT * FROM get_groups_transactions() WHERE group_cost > group;
 
-WITH
-checks AS (
-    SELECT transaction_id, sku_id, sku_discount FROM checks
-),
-products AS (
-    SELECT sku_id, group_id FROM products
-),
-transactions AS (
-    SELECT transaction_id, card_id, transaction_datetime FROM transactions
-),
-general_transactions AS ( -- all transactions for groups inside the period of bying
-    SELECT p.customer_id, p.group_id, COUNT(transaction_id) AS transactions_count
-    FROM purchase_history ph
-    JOIN (
-        SELECT customer_id, group_id, first_group_purchase_date, last_group_purchase_date
-        FROM periods ) p
-    ON ph.customer_id = p.customer_id
-    WHERE transaction_datetime::TIMESTAMP >= first_group_purchase_date::TIMESTAMP AND
-        transaction_datetime::TIMESTAMP <= last_group_purchase_date::TIMESTAMP
-    GROUP BY p.customer_id, p.group_id
-),
-group_transactions AS ( -- transactions for groups inside the period of bying where the group was bought
-    SELECT customer_id, group_id, COUNT(transaction_id) AS transactions_count
-    FROM purchase_history
-    GROUP BY customer_id, group_id
-), 
-affinity_index AS (
-    SELECT 
-        gen.customer_id, 
-        gen.group_id, 
-        gr.transactions_count::NUMERIC / gen.transactions_count AS group_affinity_index
-    FROM general_transactions gen JOIN
-        group_transactions gr ON gen.customer_id = gr.customer_id AND gen.group_id = gr.group_id
-),
-churn_rate AS (
-    SELECT 
-        p.customer_id,
+CALL import_default_dataset_mini();
+REFRESH MATERIALIZED VIEW purchase_history;
+REFRESH MATERIALIZED VIEW periods;
+
+
+
+SELECT 
+    average_frequency_deviation / group_frequency AS group_stability_index,
+    group_transactions_count / general_transactions_count AS group_affinity_index
+    analysis_formation - last_date_by_period / group_frequency AS 
+
+
+
+SELECT * FROM purchase_history
+
+SELECT 
+    customer_id,
+    group_id,
+    COUNT(transaction_id) FILTER (WHERE 
+        transaction_datetime <= last_group_purchase_date AND
+        transaction_datetime >= first_group_purchase_date)
+        AS general_transactions_count,
+    COUNT(transaction_id) FILTER (WHERE
+        purchased_group_id = group_id) AS group_transactions_count,
+    EXTRACT(EPOCH FROM MAX(analysis_formation) FILTER (WHERE 
+        row_n = 1)) AS analysis_formation_epoch,
+    EXTRACT(EPOCH FROM MAX(last_group_purchase_date) FILTER (WHERE 
+        row_n = 1)) AS last_group_purchase_epoch,
+    MAX(group_frequency) FILTER (WHERE row_n = 1) AS group_frequency,
+    AVG(ABS((next_transaction_epoch - current_transaction_epoch) / (60 * 60 * 24) - 
+        group_frequency)) FILTER (WHERE purchased_group_id = group_id)
+        AS average_frequency_deviation
+FROM (
+    SELECT
+        ph.customer_id,
+        ph.transaction_id,
+        ph.transaction_datetime,
+        ph.group_id AS purchased_group_id,
+        EXTRACT(EPOCH FROM (ph.transaction_datetime)) AS current_transaction_epoch,
+        EXTRACT(EPOCH FROM (
+            LAG(ph.transaction_datetime, -1) OVER purchase_intervals))
+            AS next_transaction_epoch,
         p.group_id,
-        (analysis_formation::DATE - last_group_purchase_date::DATE)::NUMERIC / group_frequency AS group_churn_rate 
-    FROM periods p
+        p.group_frequency,
+        p.first_group_purchase_date,
+        p.last_group_purchase_date,
+        ROW_NUMBER() OVER group_analysis AS row_n,
+        d.analysis_formation
+    FROM purchase_history ph
+    JOIN periods p ON ph.customer_id = p.customer_id
     CROSS JOIN (
         SELECT MAX(analysis_formation) AS analysis_formation
         FROM date_of_analysis_formation ) d
-),
-stability_index AS (
-    SELECT 
-        h.customer_id,
-        h.group_id,
-        AVG(ABS(h.purchase_interval - p.group_frequency) / p.group_frequency) AS group_stability_index 
-    FROM (
-        SELECT
+    WINDOW 
+        purchase_intervals AS (
+            PARTITION BY ph.customer_id, ph.group_id, p.group_id ORDER BY ph.transaction_datetime),
+        group_analysis AS (
+            PARTITION BY ph.customer_id, p.group_id ORDER BY ph.transaction_datetime)
+) accumulation
+GROUP BY customer_id, group_id;
+
+
+
+
+
+EXPLAIN ANALYSE
+WITH
+
+    -- general_transactions AS ( -- all transactions for all groups (ph.group_id) inside the period of bying certain group (p.group_id)
+    --     SELECT p.customer_id, p.group_id, COUNT(ph.transaction_id) AS transactions_count
+    --     FROM purchase_history ph
+    --     LEFT JOIN periods p ON ph.customer_id = p.customer_id
+    --     WHERE transaction_datetime::TIMESTAMP >= first_group_purchase_date::TIMESTAMP AND
+    --         transaction_datetime::TIMESTAMP <= last_group_purchase_date::TIMESTAMP
+    --     GROUP BY p.customer_id, p.group_id
+    -- ),
+
+    -- group_transactions AS ( -- all transactions for certain group (ph.group_id) inside the period of bying certain group (ph.group_id)
+    --     SELECT ph.customer_id, ph.group_id, COUNT(ph.transaction_id) AS transactions_count
+    --     FROM purchase_history ph
+    --     GROUP BY ph.customer_id, ph.group_id
+    -- ), 
+
+    -- affinity_index AS ( -- 0.0 for non-founded customer-group pairs
+    --     SELECT 
+    --         gen.customer_id,
+    --         gen.group_id,
+    --         COALESCE(gr.transactions_count, 0)::NUMERIC / gen.transactions_count AS group_affinity_index
+    --     FROM general_transactions gen
+    --     LEFT JOIN group_transactions gr ON gen.customer_id = gr.customer_id AND gen.group_id = gr.group_id
+    -- ),
+
+    -- churn_rate AS ( -- inf for non-founded customer-group pairs
+    --     SELECT 
+    --         p.customer_id,
+    --         p.group_id,
+    --         (analysis_formation::DATE - last_group_purchase_date::DATE)::NUMERIC / group_frequency AS group_churn_rate 
+    --     FROM periods p
+    --     CROSS JOIN (
+    --         SELECT MAX(analysis_formation) AS analysis_formation
+    --         FROM date_of_analysis_formation ) d
+    -- ),
+
+    -- stability_index AS ( -- null
+    --     SELECT 
+    --         h.customer_id,
+    --         h.group_id,
+    --         AVG(ABS(h.purchase_interval - p.group_frequency) / p.group_frequency) AS group_stability_index 
+    --     FROM (
+    --         SELECT
+    --             ph.customer_id, 
+    --             ph.group_id,
+    --             (LAG(ph.transaction_datetime, -1) OVER (PARTITION BY ph.customer_id, ph.group_id ORDER BY ph.transaction_datetime::TIMESTAMP))::DATE - ph.transaction_datetime::DATE AS purchase_interval
+    --         FROM purchase_history ph ) h
+    --     JOIN periods p ON h.customer_id = p.customer_id AND h.group_id = p.group_id
+    --     WHERE h.purchase_interval IS NOT NULL
+    --     GROUP BY h.customer_id, h.group_id
+    -- ),
+
+    margin AS (
+        SELECT ph.customer_id, ph.group_id, SUM(ph.group_summ_paid - ph.group_cost) AS group_margin
+        FROM get_groups_transactions() ph
+        GROUP BY ph.customer_id, ph.group_id
+    ),
+
+    distinct_discount_checks AS (
+        SELECT DISTINCT dc.transaction_id, dc.sku_id, c.customer_id, p.group_id
+        FROM (
+            SELECT transaction_id, sku_id
+            FROM checks
+            WHERE sku_discount != 0 ) dc
+        JOIN transactions t ON t.transaction_id = dc.transaction_id
+        JOIN cards c ON t.card_id = c.card_id
+        JOIN products p ON p.sku_id = dc.sku_id
+    ),
+
+    discount_share AS (
+        SELECT 
+            p.customer_id, 
+            p.group_id, 
+            ddc_count.transactions_count::NUMERIC / p.group_purchase AS group_discount_share,
+            p.group_min_discount AS group_minimum_discount
+        FROM (
+            SELECT customer_id, group_id, COUNT(transaction_id) AS transactions_count
+            FROM distinct_discount_checks ddc
+            GROUP BY customer_id, group_id ) ddc_count
+        LEFT JOIN periods p ON p.customer_id = ddc_count.customer_id AND p.group_id = ddc_count.group_id
+    ),
+
+    average_discount AS (
+        SELECT 
+            ph.group_id, 
             ph.customer_id, 
-            ph.group_id,
-            (LAG(ph.transaction_datetime, -1) OVER (PARTITION BY ph.customer_id, ph.group_id ORDER BY ph.transaction_datetime::TIMESTAMP))::DATE - ph.transaction_datetime::DATE AS purchase_interval
-        FROM purchase_history ph ) h
-    JOIN periods p ON h.customer_id = p.customer_id AND h.group_id = p.group_id
-    WHERE h.purchase_interval IS NOT NULL
-    GROUP BY h.customer_id, h.group_id
-),
-margin AS (
-    SELECT ph.customer_id, ph.group_id, SUM(ph.group_summ_paid) - SUM(ph.group_cost) AS group_margin
-    FROM get_groups_transactions() ph
-    GROUP BY ph.customer_id, ph.group_id
-),
-distinct_discount_checks AS (
-    SELECT DISTINCT dc.transaction_id, dc.sku_id, c.customer_id, p.group_id
-    FROM (
-        SELECT transaction_id, sku_id
-        FROM checks
-        WHERE sku_discount != 0 ) dc
-    JOIN transactions t ON t.transaction_id = dc.transaction_id
-    JOIN cards c ON t.card_id = c.card_id
-    JOIN products p ON p.sku_id = dc.sku_id
-),
-discount_share AS (
-    SELECT 
-        p.customer_id, 
-        p.group_id, 
-        ddc_count.transactions_count::NUMERIC / p.group_purchase AS group_discount_share,
-        p.group_min_discount AS group_minimum_discount
-    FROM (
-        SELECT customer_id, group_id, COUNT(transaction_id) AS transactions_count
-        FROM distinct_discount_checks ddc
-        GROUP BY customer_id, group_id ) ddc_count
-    LEFT JOIN periods p ON p.customer_id = ddc_count.customer_id AND p.group_id = ddc_count.group_id
-),
-average_discount AS (
-    SELECT 
-        ph.group_id, 
-        ph.customer_id, 
-        AVG(ph.group_summ_paid / ph.group_summ) AS group_average_discount
-    FROM (
-        SELECT DISTINCT transaction_id, group_id, customer_id
-        FROM distinct_discount_checks ) ddc
-    JOIN purchase_history ph ON 
-        ph.transaction_id = ddc.transaction_id AND 
-        ph.customer_id = ddc.customer_id AND 
-        ph.group_id = ddc.customer_id
-    GROUP BY ph.customer_id, ph.group_id
-)
+            AVG(ph.group_summ_paid / ph.group_summ) AS group_average_discount
+        FROM (
+            SELECT DISTINCT transaction_id, group_id, customer_id
+            FROM distinct_discount_checks ) ddc
+        LEFT JOIN (
+            SELECT transaction_id, customer_id, group_id, group_summ_paid, group_summ
+            FROM purchase_history ) ph ON 
+            ph.transaction_id = ddc.transaction_id AND 
+            ph.customer_id = ddc.customer_id AND 
+            ph.group_id = ddc.customer_id
+        GROUP BY ph.customer_id, ph.group_id
+    )
+
 SELECT
-    customer_id,
-    group_id,
-    group_affinity_index,
-    group_churn_rate,
-    group_stability_index,
-    group_margin,
-    COALESCE(group_discount_share, 0) AS group_discount_share,
-    COALESCE(group_minimum_discount, 0) AS group_minimum_discount,
-    COALESCE(group_average_discount, 0) AS group_average_discount
-FROM affinity_index
-NATURAL JOIN churn_rate
-NATURAL JOIN stability_index
-NATURAL JOIN margin
-NATURAL LEFT JOIN discount_share
-NATURAL LEFT JOIN average_discount;
+    ai.customer_id,
+    ai.group_id,
+    ai.group_affinity_index,
+    cr.group_churn_rate,
+    si.group_stability_index,
+    m.group_margin,
+    COALESCE(ds.group_discount_share, 0) AS group_discount_share,
+    COALESCE(ds.group_minimum_discount, 0) AS group_minimum_discount,
+    COALESCE(ad.group_average_discount, 0) AS group_average_discount
+FROM affinity_index ai
+LEFT JOIN churn_rate cr ON ai.customer_id = cr.customer_id AND ai.group_id = cr.group_id
+LEFT JOIN stability_index si ON ai.customer_id = cr.customer_id AND ai.group_id = cr.group_id
+LEFT JOIN margin m ON ai.customer_id = si.customer_id AND ai.group_id = si.group_id
+LEFT JOIN discount_share ds ON ai.customer_id = ds.customer_id AND ai.group_id = ds.group_id
+LEFT JOIN average_discount ad ON ai.customer_id = ad.customer_id AND ai.group_id = ad.group_id;
+
+SELECT * FROM periods; -- 55 шт
+
+
+SELECT * FROM retail_analitycs_config;
+CALL export_to_tsv('retail_analitycs_config');
+
+UPDATE retail_analitycs_config SET setting =
+'by all transactions' WHERE name = 'groups_margin_calculation_method'
