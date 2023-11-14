6c37389175760b9ee20275fa580c4d2fa6e7e364
Timofeeva Anna <dedelmir@student.21-school.ru>
2023-11-15 02:32:53 +0300

    fix: #21 + style + optimization

--- src/part2_3_periods.sql
+++ src/part2_3_periods.sql
@@ -1,41 +1,46 @@
 \connect -reuse-previous=on "dbname=retail_analytics user=retail_user";
 
 DROP VIEW IF EXISTS periods;
-
-CREATE OR REPLACE VIEW periods AS
+CREATE VIEW periods AS
     WITH
-    raw_data_for_periods AS (
-        SELECT t.transaction_id, customer_id, group_id, sku_discount, sku_summ, transaction_datetime
-        FROM checks JOIN
-            transactions t ON checks.transaction_id = t.transaction_id JOIN
-            products ON checks.sku_id = products.sku_id JOIN
-            cards ON cards.card_id = t.card_id
-    ),
-    transactions_count AS (
-        SELECT
-            customer_id,
-            group_id,
-            COUNT(transaction_id) AS group_purchase
-        FROM purchase_history
-        GROUP BY customer_id, group_id
-    )
+
+        raw_data_for_periods AS (
+            SELECT t.transaction_id, customer_id, group_id, sku_discount, sku_summ, transaction_datetime
+            FROM checks
+            LEFT JOIN transactions t ON checks.transaction_id = t.transaction_id
+            LEFT JOIN products ON checks.sku_id = products.sku_id
+            LEFT JOIN cards ON cards.card_id = t.card_id
+        ),
+
+        transactions_count AS (
+            SELECT
+                customer_id,
+                group_id,
+                COUNT(transaction_id) AS group_purchase
+            FROM purchase_history
+            GROUP BY customer_id, group_id
+        )
+
     SELECT
         rd.customer_id,
         rd.group_id,
         rd.first_group_purchase_date,
         rd.last_group_purchase_date, 
-        tc.group_purchase,
-        (rd.last_group_purchase_date::DATE - rd.first_group_purchase_date::DATE + 1)::NUMERIC / tc.group_purchase AS group_frequency,
-        group_min_discount
+        tc.group_purchase::NUMERIC,
+        (EXTRACT(EPOCH FROM rd.last_group_purchase_date) - 
+            EXTRACT(EPOCH FROM rd.first_group_purchase_date))::NUMERIC /
+            tc.group_purchase AS group_frequency,
+        COALESCE(rd.group_min_discount, 0)::NUMERIC AS group_min_discount
     FROM (
         SELECT
             customer_id,
             group_id,
             MIN(transaction_datetime) AS first_group_purchase_date,
             MAX(transaction_datetime) AS last_group_purchase_date,
-            MIN(sku_discount / sku_summ) AS group_min_discount
+            MIN(sku_discount / sku_summ) FILTER (
+                WHERE sku_discount != 0 ) AS group_min_discount
         FROM raw_data_for_periods
-        GROUP BY customer_id, group_id) rd
-    JOIN transactions_count tc ON tc.customer_id = rd.customer_id AND tc.group_id = rd.group_id;
+        GROUP BY customer_id, group_id ) rd
+    LEFT JOIN transactions_count tc ON tc.customer_id = rd.customer_id AND tc.group_id = rd.group_id;
 
 -- SELECT * FROM periods;
