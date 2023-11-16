add656b5c921535028900b9b756c40606c862178
Timofeeva Anna <dedelmir@student.21-school.ru>
2023-11-16 19:48:36 +0300

    fix: #28

--- src/part2_3_periods.sql
+++ src/part2_3_periods.sql
@@ -29,7 +29,7 @@ CREATE VIEW periods AS
         tc.group_purchase::NUMERIC,
         (EXTRACT(EPOCH FROM rd.last_group_purchase_date) - 
             EXTRACT(EPOCH FROM rd.first_group_purchase_date))::NUMERIC /
-            tc.group_purchase AS group_frequency,
+            tc.group_purchase / (24*60*60) AS group_frequency,
         COALESCE(rd.group_min_discount, 0)::NUMERIC AS group_min_discount
     FROM (
         SELECT
