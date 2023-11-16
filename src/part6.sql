e0e868526b927f19f536bfe937964cd54c93b11e
Kadyr Atakhan <kadyr.atakhanov@gmail.com>
2023-11-16 17:11:55 +0300

    feat: group_selection, sku_with_max_margin

--- src/part6.sql
+++ src/part6.sql
@@ -1,11 +1,11 @@
 \connect -reuse-previous=on "dbname=retail_analytics user=retail_user"; 
 
 CREATE FUNCTION get_personal_offers_aimed_at_cross_selling(
-    number_of_groups INT,
-    max_churn_index NUMERIC,
-    max_consumption_stab_index NUMERIC,
-    max_sku_share NUMERIC,              -- (in percent)
-    allowable_margin_share NUMERIC      -- (in percent)
+    IN number_of_groups INT,
+    IN max_churn_index NUMERIC,
+    IN max_consumption_stab_index NUMERIC,
+    IN max_sku_share_percentage NUMERIC,
+    IN allowable_margin_share_percentage NUMERIC
 ) RETURNS TABLE (
     customer_id BIGINT,
     sku_name VARCHAR,
@@ -18,12 +18,61 @@ END; $$
 LANGUAGE plpgsql;
 
 
-WITH group_selection AS (
-    SELECT * 
-    FROM groups 
-    WHERE group_churn_rate > 1000           -- 1000 - value for test
-        AND group_stability_index < 0.70    -- 0.70 - value for test
-    LIMIT 100
+WITH
+-- Group selection
+--–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+group_selection AS (
+    SELECT *
+    FROM (
+        SELECT 
+            gr.customer_id,
+            gr.group_id,
+            group_affinity_index,
+            gr.group_churn_rate,
+            gr.group_stability_index,
+            ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY group_id ASC) AS r
+        FROM groups gr
+        WHERE 
+            group_churn_rate > 500
+            AND
+            group_stability_index < 0.90
+        ORDER BY customer_id, group_affinity_index DESC
+    ) rg
+    WHERE rg.r <= 3
+    LIMIT 20
 )
 
-SELECT * FROM group_selection;
+SELECT DISTINCT ON (pm.customer_id, pm.group_id)
+    pm.customer_id,
+    pm.group_id,
+    pm.store_id,
+    pm.product_margin
+    -- pm.max_margin
+FROM (
+    SELECT 
+        gs.customer_id,
+        gs.group_id,
+        c.customer_primary_store AS store_id,
+        (sp.sku_retail_price - sp.sku_purchase_price) AS product_margin
+        -- max(sp.sku_retail_price - sp.sku_purchase_price) OVER (PARTITION BY pm.customer_id, pm.group_id) AS max_margin
+    FROM group_selection gs
+    JOIN customers c ON c.customer_id = gs.customer_id
+    JOIN stores_products sp ON sp.store_id = c.customer_primary_store
+) pm
+ORDER BY pm.customer_id, pm.group_id, pm.store_id, pm.product_margin DESC;
+
+-- Determination of SKU with maximum margin
+--–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+-- Определение SKU с максимальной маржой. 
+-- В каждой группе определяется SKU с максимальной маржой (в рублях).
+
+-- Для этого, по основному магазину клиента 
+-- из розничной цены товара (SKU_Retail_Price) 
+-- вычитается его закупочная стоимость (SKU_Purchase_Price) 
+-- для всех SKU данной группы, представленных в магазине, 
+
+-- после чего выбирается одно SKU с максимальным значением указанной разницы.
+
+-- Customers::Customer_Primary_Store
+-- stores_products::SKU_Retail_Price
+-- stores_products::SKU_Purchase_Price
