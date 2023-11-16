647b7167b80b43ea697443cef511d6deece19c90
Kadyr Atakhan <kadyr.atakhanov@gmail.com>
2023-11-16 21:09:01 +0300

    temp part6

--- src/part6.sql
+++ src/part6.sql
@@ -19,60 +19,79 @@ LANGUAGE plpgsql;
 
 
 WITH
--- Group selection
---–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-group_selection AS (
-    SELECT *
-    FROM (
+    -- Group selection
+    --–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    group_selection AS (
+        SELECT *
+        FROM (
+            SELECT 
+                gr.customer_id,
+                gr.group_id,
+                group_affinity_index,
+                gr.group_churn_rate,
+                gr.group_stability_index,
+                ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY group_id ASC) AS r
+            FROM groups gr
+            WHERE 
+                group_churn_rate > 500
+                AND
+                group_stability_index < 0.90
+            ORDER BY customer_id, group_affinity_index DESC
+        ) rg
+        WHERE rg.r <= 3
+        LIMIT 20
+    ),
+    -- Determination of SKU with maximum margin
+    --–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    sku_with_max_margin AS (
+        SELECT DISTINCT ON (pm.customer_id, pm.group_id)
+            pm.customer_id,
+            pm.group_id,
+            pm.store_id,
+            pm.sku_id,
+            pm.product_margin AS max_margin
+        FROM (
+            SELECT 
+                gs.customer_id,
+                gs.group_id,
+                c.customer_primary_store AS store_id,
+                sp.sku_id,
+                (sp.sku_retail_price - sp.sku_purchase_price) AS product_margin
+            FROM group_selection gs
+            JOIN customers c ON c.customer_id = gs.customer_id
+            JOIN stores_products sp ON sp.store_id = c.customer_primary_store
+        ) pm
+        ORDER BY 
+            pm.customer_id, 
+            pm.group_id, 
+            pm.store_id, 
+            pm.product_margin DESC
+    ),
+    -- SKU share in a group
+    --–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    -- Определяется доля транзакций, в которых присутствует анализируемое SKU. 
+    
+    -- Для этого количество транзакций, содержащих данный SKU, 
+    -- делится на количество транзакций, содержащих группу в целом
+    
+    -- SKU используется для формирования предложения только в том случае,
+    -- если получившееся значение не превышает заданного пользователем значения.
+    sku_share_in_a_group AS (
         SELECT 
-            gr.customer_id,
-            gr.group_id,
-            group_affinity_index,
-            gr.group_churn_rate,
-            gr.group_stability_index,
-            ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY group_id ASC) AS r
-        FROM groups gr
-        WHERE 
-            group_churn_rate > 500
-            AND
-            group_stability_index < 0.90
-        ORDER BY customer_id, group_affinity_index DESC
-    ) rg
-    WHERE rg.r <= 3
-    LIMIT 20
-)
+            *
+        FROM checks ch
+        LEFT JOIN transactions t ON t.transaction_id = ch.transaction_id
+        LEFT JOIN cards c ON t.card_id = c.card_id
+        LEFT JOIN sku_with_max_margin swmm ON c.customer_id = swmm.customer_id
+            AND ch.sku_id = swmm.sku_id
+        
+        -- JOIN transactions t
+        -- -- WHERE swmm.customer_id = 16
+        -- LIMIT 100
+    )
 
-SELECT DISTINCT ON (pm.customer_id, pm.group_id)
-    pm.customer_id,
-    pm.group_id,
-    pm.store_id,
-    pm.product_margin
-    -- pm.max_margin
-FROM (
-    SELECT 
-        gs.customer_id,
-        gs.group_id,
-        c.customer_primary_store AS store_id,
-        (sp.sku_retail_price - sp.sku_purchase_price) AS product_margin
-        -- max(sp.sku_retail_price - sp.sku_purchase_price) OVER (PARTITION BY pm.customer_id, pm.group_id) AS max_margin
-    FROM group_selection gs
-    JOIN customers c ON c.customer_id = gs.customer_id
-    JOIN stores_products sp ON sp.store_id = c.customer_primary_store
-) pm
-ORDER BY pm.customer_id, pm.group_id, pm.store_id, pm.product_margin DESC;
+SELECT * FROM sku_share_in_a_group;
 
--- Determination of SKU with maximum margin
---–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
--- Определение SKU с максимальной маржой. 
--- В каждой группе определяется SKU с максимальной маржой (в рублях).
-
--- Для этого, по основному магазину клиента 
--- из розничной цены товара (SKU_Retail_Price) 
--- вычитается его закупочная стоимость (SKU_Purchase_Price) 
--- для всех SKU данной группы, представленных в магазине, 
-
--- после чего выбирается одно SKU с максимальным значением указанной разницы.
-
--- Customers::Customer_Primary_Store
--- stores_products::SKU_Retail_Price
--- stores_products::SKU_Purchase_Price
+SELECT * FROM checks;
+SELECT * FROM transactions;
+SELECT * FROM cards;
