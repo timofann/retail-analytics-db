3a447e880253addef39c8a4bd1fb5e3a213d1c48
timofann <dedelmir@student.21-school.ru>
2023-11-18 05:58:06 +0300

    fix: #24 style + refactoring

--- src/part6.sql
+++ src/part6.sql
@@ -1,17 +1,6 @@
 \connect -reuse-previous=on "dbname=retail_analytics user=retail_user"; 
 
--- TODO delete
-DROP FUNCTION IF EXISTS round_discount CASCADE;
-CREATE OR REPLACE FUNCTION round_discount(discount NUMERIC)
-    RETURNS NUMERIC
-AS $$
-BEGIN
-    RETURN ((FLOOR(discount / 5)) + (discount % 5 != 0)::INT) * 5.0;
-END; $$
-LANGUAGE plpgsql;
-
 DROP FUNCTION IF EXISTS get_personal_offers_aimed_at_cross_selling CASCADE;
-
 CREATE FUNCTION get_personal_offers_aimed_at_cross_selling(
     IN number_of_groups INT,
     IN max_churn_index NUMERIC,
@@ -40,8 +29,7 @@ BEGIN
                     WHERE 
                         group_churn_rate <= max_churn_index AND
                         group_churn_rate IS NOT NULL AND
-                        group_stability_index < max_consumption_stab_index
-                ) rg
+                        group_stability_index < max_consumption_stab_index ) rg
                 WHERE rg.r <= number_of_groups
             ),
 
@@ -63,8 +51,7 @@ BEGIN
                         (sp.sku_retail_price - sp.sku_purchase_price) AS product_margin
                     FROM group_selection gs
                     JOIN customers c ON c.customer_id = gs.customer_id
-                    JOIN stores_products sp ON sp.store_id = c.customer_primary_store
-                ) pm
+                    JOIN stores sp ON sp.transaction_store_id = c.customer_primary_store ) pm
                 ORDER BY 
                     pm.customer_id,
                     pm.group_id,
@@ -77,12 +64,9 @@ BEGIN
                     sk.sku_id,
                     COUNT (transaction_id) as transactions_count
                 FROM (
-                    SELECT 
-                        sku_id
+                    SELECT swmm.sku_id
                     FROM sku_with_max_margin swmm
-                    LEFT JOIN cards c ON c.customer_id = swmm.customer_id
-                    GROUP BY sku_id
-                ) sk
+                    GROUP BY sku_id ) sk
                 JOIN checks c ON sk.sku_id = c.sku_id
                 GROUP BY sk.sku_id
             ),
@@ -100,15 +84,16 @@ BEGIN
             pr.sku_name,
             round_discount(p.group_min_discount) AS offer_discount_depth
         FROM sku_with_max_margin swmm
-        LEFT JOIN products pr ON pr.sku_id = swmm.sku_id
+        LEFT JOIN product_grid pr ON pr.sku_id = swmm.sku_id
         LEFT JOIN sku_transactions st ON swmm.sku_id = st.sku_id
         LEFT JOIN group_transactions gt ON swmm.group_id = gt.group_id
         LEFT JOIN periods p ON p.customer_id = swmm.customer_id AND p.group_id = swmm.group_id
-        WHERE (st.transactions_count::NUMERIC / gt.group_transactions_count) < (max_sku_share_percentage / 100)
-            AND round_discount(p.group_min_discount) >= (((allowable_margin_share_percentage / 100) * swmm.max_margin::NUMERIC) / swmm.sku_retail_price)
-    );
+        WHERE
+            (st.transactions_count::NUMERIC / gt.group_transactions_count) < 
+                (max_sku_share_percentage / 100) AND 
+            (((allowable_margin_share_percentage / 100) * swmm.max_margin::NUMERIC) / swmm.sku_retail_price) <
+                round_discount(p.group_min_discount) );
 END; $$
 LANGUAGE plpgsql;
 
-
-SELECT * FROM get_personal_offers_aimed_at_cross_selling(5, 3, 0.5, 100, 30)
+-- SELECT * FROM get_personal_offers_aimed_at_cross_selling(5, 3, 0.5, 100, 30);
